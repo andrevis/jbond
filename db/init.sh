4d85c283a0cf4af3cd95b1538992c8f9057ed0a4
andrevis <anlkltln@yandex.ru>
2025-06-27 15:10:39 +0300

    fixip

---
+++ db/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+db_name=$1
+db_user=$2
+db_pass=$3
+
+psql -U postgres -c "CREATE USER ${db_name} PASSWORD '${db_pass}';"
+psql -U postgres -c "CREATE DATABASE ${db_name} OWNER=${db_user};"
